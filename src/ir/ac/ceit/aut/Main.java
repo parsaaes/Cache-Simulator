345e8a998039be931349d072af6011a9c0444621
parsaaes <parsaaes@gmail.com>
2018-04-16 22:18:25 +0430

    Some changes in address generation

--- src/ir/ac/ceit/aut/Main.java
+++ src/ir/ac/ceit/aut/Main.java
@@ -1,70 +1,18 @@
 package ir.ac.ceit.aut;
 
 import ir.ac.ceit.aut.address.AddressGenerator;
-import ir.ac.ceit.aut.memory.DirectMapCache;
-import ir.ac.ceit.aut.memory.FACache;
-import ir.ac.ceit.aut.memory.Word;
 
 public class Main {
 
     public static void main(String[] args) {
+
         AddressGenerator addressGenerator = new AddressGenerator();
         addressGenerator.generate("in2");
+
         Simulator simulator1 = new Simulator(12,4);
         simulator1.run(false);
         Simulator simulator2 = new Simulator(12,4);
         simulator2.run(true);
-//
-//        FACache faCache = new FACache(8,4);
-//        System.out.println(faCache.i("00000000000000000000000000000000"));
 
-//
-//        FACache dm = new FACache(8,4);
-//
-//
-//        for(int i = 0; i < 16; i++){
-//            String w = "";
-//            for(int j = 0; j < i; j++){
-//                w += "0";
-//            }
-//            for(int k = 0; k < 32 - i; k++){
-//                w+="1";
-//            }
-//            System.out.println(w + "-> " + w.length());
-//            dm.requestWord(w);
-//        }
-//
-//
-//        for (int i = 0; i < dm.getWordArray().length; i++) {
-//            System.out.println(i + "- " + dm.getWordArray()[i].getData() + " "+ dm.getWordArray()[i].isValid() + ": " + dm.getUsage()[(int) (i / Math.pow(2,dm.getBlockSizeBitCount()))]);
-//        }
-//
-//        System.out.println("requested->");
-//        dm.requestWord("00000000001111111111111111111110");
-//
-//        for (int i = 0; i < dm.getWordArray().length; i++) {
-//            System.out.println(i + "- " + dm.getWordArray()[i].getData() + " "+ dm.getWordArray()[i].isValid() + ": " + dm.getUsage()[(int) (i / Math.pow(2,dm.getBlockSizeBitCount()))]);
-//        }
-//
-//        System.out.println("requested->");
-//        dm.requestWord("00000000001111111110011111111110");
-//
-//        for (int i = 0; i < dm.getWordArray().length; i++) {
-//            System.out.println(i + "- " + dm.getWordArray()[i].getData() + " "+ dm.getWordArray()[i].isValid() + ": " + dm.getUsage()[(int) (i / Math.pow(2,dm.getBlockSizeBitCount()))]);
-//        }
-//
-//        System.out.println("requested->");
-//        dm.requestWord("01100000001111111111111111111110");
-//
-//        for (int i = 0; i < dm.getWordArray().length; i++) {
-//            System.out.println(i + "- " + dm.getWordArray()[i].getData() + " "+ dm.getWordArray()[i].isValid() + ": " + dm.getUsage()[(int) (i / Math.pow(2,dm.getBlockSizeBitCount()))]);
-//        }
-//
-//        System.out.println("requested->");
-//        dm.requestWord("00000000001111111111111111111010");
-//
-//        for (int i = 0; i < dm.getWordArray().length; i++) {
-//            System.out.println(i + "- " + dm.getWordArray()[i].getData() + " "+ dm.getWordArray()[i].isValid() + ": " + dm.getUsage()[(int) (i / Math.pow(2,dm.getBlockSizeBitCount()))]);
-//        }
     }
 }
--- src/ir/ac/ceit/aut/address/AddressGenerator.java
+++ src/ir/ac/ceit/aut/address/AddressGenerator.java
@@ -22,16 +22,31 @@ public class AddressGenerator {
         for(int i = 0; i < startOfLocality; i++){
             addresses[i] = toBinString( (Math.abs(random.nextLong()) % (MAX - MIN)) + MIN);
         }
+
         for (int i = startOfLocality; i< addresses.length; ) {
-            String number = addresses[Math.abs(random.nextInt())% startOfLocality];
+            String number = addresses[Math.abs(random.nextInt()) % (startOfLocality / 5 * (int)Math.ceil(( 2d * i / COUNT)))];
             addresses[i++] = number;
             if ((Math.abs(random.nextInt()) % 10) == 0) {
-                long lengthOfLocality = Math.min(Math.abs(random.nextInt()) % 10 + 5,(MAX-toLong(number)));
+                long lengthOfLocality = Math.min(Math.abs(random.nextInt()) % 5 + 3,(MAX-toLong(number)));
                 for (int j = 0; j < lengthOfLocality && i < addresses.length; j++) {
                     addresses[i++] = String.valueOf(toBinString(toLong(number)+j));
                 }
             }
         }
+
+        /* another method: */
+//        for (int i = startOfLocality; i< addresses.length; ) {
+//            String number = addresses[Math.abs(random.nextInt())% startOfLocality];
+//            addresses[i++] = number;
+//            if ((Math.abs(random.nextInt()) % 10) == 0) {
+//                long lengthOfLocality = Math.min(Math.abs(random.nextInt()) % 10 + 5,(MAX-toLong(number)));
+//                for (int j = 0; j < lengthOfLocality && i < addresses.length; j++) {
+//                    addresses[i++] = String.valueOf(toBinString(toLong(number)+j));
+//                }
+//            }
+//        }
+        /* end of method */
+
 
 //        for (int i = 0; i < addresses.length; i++) {
 //            String address = addresses[i];
