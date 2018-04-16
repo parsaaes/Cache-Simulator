cf9e2ca63c8680e0fc43e4eeb7bdc9b4b6623148
parsaaes <parsaaes@gmail.com>
2018-04-16 23:16:31 +0430

    Nothing :|

--- src/ir/ac/aut/ceit/Main.java
+++ src/ir/ac/aut/ceit/Main.java
@@ -1,13 +1,20 @@
 package ir.ac.aut.ceit;
 
 
+import ir.ac.aut.ceit.address.AddressGenerator;
+
 public class Main {
 
     public static void main(String[] args) {
+
+//        AddressGenerator addressGenerator = new AddressGenerator();
+//        addressGenerator.generate("test");
+
         String instructions = "inputs/inp5.txt";
         Simulator simulator1 = new Simulator(12,8,4,instructions);
         simulator1.run(false);
         Simulator simulator2 = new Simulator(12,8,4,instructions);
         simulator2.run(true);
+
     }
 }
