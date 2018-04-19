9ff2c8feb4dd6cf3a13e177f4378c29ced920ece
parsaaes <parsaaes@gmail.com>
2018-04-19 21:24:23 +0430

    Nothing 🤔

--- src/ir/ac/aut/ceit/Main.java
+++ src/ir/ac/aut/ceit/Main.java
@@ -7,10 +7,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        AddressGenerator addressGenerator = new AddressGenerator();
-        addressGenerator.generate("inp5d");
+//        AddressGenerator addressGenerator = new AddressGenerator();
+//        addressGenerator.generate("inp5d");
 
-        String instructions = "inputs/inp5d.txt";
+        String instructions = "inputs/inp5.txt";
         Simulator simulator1 = new Simulator(12,8,4,instructions);
         simulator1.run(false);
         Simulator simulator2 = new Simulator(12,8,4,instructions);
